f93e1f78471ce691a13b63fdff4e6100c1d3eaca
Bodhik <bodhik@gmail.com>
2024-09-21 13:39:56 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3-bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
-  bucket_prefix = ${var.prefix}-hashi
+  bucket_prefix = "${var.prefix}-hashi"
   bucket = "${bucket_prefix}-s3-bucket"
   acl    = "private"
   }
